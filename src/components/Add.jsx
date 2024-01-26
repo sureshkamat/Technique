6e4f1320d0a873a98a4dc6a2f0407feb3cfb0f86
Suresh Kamat <sureshkamat108@gmail.com>
2024-01-26 21:43:02 +0530

    UI Change

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { Input } from '@chakra-ui/react'
 export const Add = () => {
     const [user,setUser]=useState ({
         name:"",
@@ -38,12 +39,12 @@ const handleData=(e)=>{
   return (
     <div className='addForm'>
         <form onSubmit={handleData}>
-        <input type="text" value={user.name} placeholder='Enter Name' onChange={(e)=>{setUser({...user,name:e.target.value})}} />
-        <input type="text" value={user.username} placeholder='Enter UserName' onChange={(e)=>{setUser({...user,username:e.target.value})}}/>
-        <input type="text" value={user.email} placeholder='Enter Email' onChange={(e)=>{setUser({...user,email:e.target.value})}}/>
-        <input type="text" value={user.company.name} placeholder='Enter company' onChange={(e)=>{setUser({...user,company:{name:e.target.value}})}}/>
-        <input type="text" value={user.website} placeholder='Enter Website' onChange={(e)=>{setUser({...user,website:e.target.value})}}/>
-        <input type="text" value={user.phone} placeholder='Enter Phone Number' onChange={(e)=>{setUser({...user,phone:e.target.value})}}/>
+        <Input type="text" value={user.name} placeholder='Enter Name' onChange={(e)=>{setUser({...user,name:e.target.value})}} />
+        <Input type="text" value={user.username} placeholder='Enter UserName' onChange={(e)=>{setUser({...user,username:e.target.value})}}/>
+        <Input type="text" value={user.email} placeholder='Enter Email' onChange={(e)=>{setUser({...user,email:e.target.value})}}/>
+        <Input type="text" value={user.company.name} placeholder='Enter company' onChange={(e)=>{setUser({...user,company:{name:e.target.value}})}}/>
+        <Input type="text" value={user.website} placeholder='Enter Website' onChange={(e)=>{setUser({...user,website:e.target.value})}}/>
+        <Input type="text" value={user.phone} placeholder='Enter Phone Number' onChange={(e)=>{setUser({...user,phone:e.target.value})}}/>
         <input type='submit' value="Add User" className='Add'/>
         </form>
     </div>
--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, Link } from 'react-router-dom';
-
+import { Input } from '@chakra-ui/react'
 export const Edit = () => {
     const { id } = useParams();
     const [user, setUser] = useState({
@@ -44,11 +44,11 @@ export const Edit = () => {
         <div className='addForm'>
             <h3>Update Details of UserId : {id}  Here or <Link to="/"><button>Go Back</button></Link></h3>
             <form onSubmit={handleEdit}>
-                <input type="text" value={user.name} placeholder='Enter Name' onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
-                <input type="text" value={user.username} placeholder='Enter UserName' onChange={(e) => { setUser({ ...user, username: e.target.value }) }} />
-                <input type="text" value={user.email} placeholder='Enter Email' onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
-                <input type="text" value={user.company.name} placeholder='Enter company' onChange={(e) => { setUser({ ...user, company: { name: e.target.value } }) }} />
-                <input type="text" value={user.website} placeholder='Enter Website' onChange={(e) => { setUser({ ...user, website: e.target.value }) }} />
+                <Input type="text" value={user.name} placeholder='Enter Name' onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
+                <Input type="text" value={user.username} placeholder='Enter UserName' onChange={(e) => { setUser({ ...user, username: e.target.value }) }} />
+                <Input type="text" value={user.email} placeholder='Enter Email' onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+                <Input type="text" value={user.company.name} placeholder='Enter company' onChange={(e) => { setUser({ ...user, company: { name: e.target.value } }) }} />
+                <Input type="text" value={user.website} placeholder='Enter Website' onChange={(e) => { setUser({ ...user, website: e.target.value }) }} />
                 <input type='submit' value="Update User" className="edit"/>
             </form>
         </div>
